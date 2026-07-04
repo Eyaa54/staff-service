e358ce5e5de600c2bb709f010a06b551c9388087
Eya Gharby <ayagharby54@gmail.com>
2026-07-04 14:12:02 +0100

    initial - staff service

--- src/main/java/com/hospital/staffservice/messaging/ForumEventListener.java
+++ src/main/java/com/hospital/staffservice/messaging/ForumEventListener.java
@@ -3,12 +3,13 @@ package com.hospital.staffservice.messaging;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+// Ecoute les messages publies par le service de mon collegue.
 @Component
 public class ForumEventListener {
 
     @KafkaListener(topics = "forum.post.created", groupId = "my-service-group")
     public void onPostCreated(String message) {
-        System.out.println("New post event: " + message);
-        // ici tu peux reagir au message recu
+        System.out.println("📩 Message recu depuis Kafka : " + message);
+        // ici tu peux reagir au message si besoin
     }
 }
